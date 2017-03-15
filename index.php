79e5f7a3c8e0685c9df247f5519ef20e1dc9eb8f
carlosrodriguez96 <esemismo96@gmail.com>
2017-03-15 07:06:42 -0500

    Update index.php
    esta es la modificación con angular

--- index.php
+++ index.php
@@ -6,42 +6,76 @@
 */
  -->
 <!DOCTYPE html>
-<html lang="ES">
+<html lang="ES" ng-app="acumuladorApp">
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <head>
 	<title></title>
+	<script src="js/angular.min.js"></script>
 	<meta charset="utf-8">
 	<?php 
 		include 'class/BD.php'; //trae las funciones de la pagina BD.php
 		$nuevo_obj=new BD();	// llama la clase BD
-			echo $nuevo_obj->estilos("bootstrap"); // esta es la funcion de los diferentes estilos para que esta funcion funcione hay que tener boostrap.
+			echo $nuevo_obj->estilos("bootstrap"); // trae la funcion estilos de la clase
 	?>
 </head>
 <body>
 	<div class="container-fluid">
+	<div ng-controller="acumuladorAppCtrl">		
 		<div class="row">
-			<?php  echo $nuevo_obj->encabezado("Mi Clinica Veterinaria"); ?> 
+			<?php  echo $nuevo_obj->encabezado("Diagnostico","de enfermedades"); ?> 
 		</div>
 		<div class="row">
 			<div class="col-xs-12 col-md-3 col-lg-3 ">
-				<div class="panel panel-default">
-				  <div class="panel-body"  style="max-height: 10;overflow-y: scroll;">
+				
+              
+				<center>
 				
 				<?php 	
 					echo $nuevo_obj->traer_informacion("sintoma","tb_sintomas","id_sintomas","sintoma","get","ver.php"); // trae la información a mostrar.
 				?>
-				  </div>
-				 </div>
-			</div>	
-			<div class="col-xs-12 col-md-3 col-lg-5 ">
-			<?php
-				echo $nuevo_obj->leer_campo( $nuevo_obj->consultar_tablas("tb_enfermedades.enfermedad","distinct"),"<th>Enfermedades</th>"); //lee y consulta las tablas.
-				
+                </center>
 
-			 ?>
+				  
+			</div>	
+			<div class="col-xs-12 col-md-8 col-lg-8 ">
+			<center><div id="carga"> </div></center>
+			<div ng-repeat="x in campos">
+			<?php 	
+					echo $nuevo_obj->imprimir("{{x.abc}}"); // trae la información a mostrar.
+
+				?>
+		 	 
+            <table class="table table-hover table-bordered">
+                <thead>
+                  <tr>
+                    <th>Enfermedad Posible</th>
+                    <th>Sintomas en esta Enfermedad</th>
+                    <th>Sintomas encontrados</th>
+                  </tr>
+                </thead>
+                 <tbody>
+                  <tr>
+                    <td>{{ x.Enfermedad }}</td>
+                    <td>{{ x.conteo_total }}</td>
+                    <td>{{ x.conteo_sintomas }}</td>
+
+                  </tr>
+                  </tbody>
+            </table>      
+                                  
+        </div>
+				 
 			</div>
 		</div>
+		</div>
 	</div>
+	<script type="text/javascript" src="js/mi_js.js"></script>
+	
+	 
+
+
 </body>
 </html>
+
+
 
